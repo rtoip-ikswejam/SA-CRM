f7fbb0bf76d50e6a9248ef01ef2bbaaec2c17d93
rtoip-ikswejam <rtoip.ikswejam@gmail.com>
2026-04-26 21:35:33 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -16,7 +16,7 @@
  * BACKEND_URL: wklej URL swojego Apps Script Web App (krok 8 README).
  */
 
-const BACKEND_URL = 'TUTAJ_WKLEJ_URL_APPS_SCRIPT_WEB_APP';
+const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbxT4rS1YnAhdBTBS5FkgOu5iq_p3u7NmChw7p_fH8SnecD7pOpj_tf0BykbP7HlU80/exe';
 
 const Api = (function() {
 
